29e441425b9e6211dad4005ad2919f74c83a88d5
Sonali Goyal <sonali.goyal@harness.io>
2022-06-06 14:15:57 +0530

    feat: [feat/cd-onboarding]: Added header and footer for wizards

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -3643,6 +3643,8 @@ export interface StringsMap {
   'cd.featureRestriction.banners.serviceWarningActive': string
   'cd.filePathPlaceholder': string
   'cd.filePathRequired': string
+  'cd.getStartedWithCD.serverless': string
+  'cd.getStartedWithCD.workloadDeploy': string
   'cd.gitFetchTypeRequired': string
   'cd.gitOps': string
   'cd.gitOpsWizardTitle': string
